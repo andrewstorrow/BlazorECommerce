09240a6f1a2f6f5f245ef9a466dbd91f97e32c98
Andrew Storrow <andrew.storrow@gmail.com>
2023-05-28 12:32:33 -0500

    Added Product List page and api controller

--- BlazorECommerce/Shared/Product.cs
+++ BlazorECommerce/Shared/Product.cs
@@ -11,5 +11,7 @@ namespace BlazorECommerce.Shared
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
     }
 }
